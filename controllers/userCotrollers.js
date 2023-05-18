d99fe5071cbc229be95a1f50391c0bfa506ddddc
tanjidulahad <ahad@sigaramtech.com>
2023-05-18 21:12:36 +0600

    normal authentication done

--- controllers/userCotrollers.js
+++ controllers/userCotrollers.js
@@ -1,4 +1,5 @@
 const User = require("../model/User")
+const bcryptjs = require("bcryptjs")
 
 const userSignupController = async (req, res, next) => {
     const { firstname, lastname, email, password } = req.body
@@ -6,20 +7,22 @@ const userSignupController = async (req, res, next) => {
     let existinguser
 
     try {
-        existinguser= await User.findOne({email})
+        existinguser = await User.findOne({ email })
     } catch (error) {
         console.log(error)
     }
 
-    if(existinguser){
-        return res.status(400).json({message:"user already exist. please signin"})
+    if (existinguser) {
+        return res.status(400).json({ message: "user already exist. please signin" })
     }
+
+    const hashedpassword = bcryptjs.hashSync(password, 8)
 
     const user = new User({
         firstname,
         lastname,
         email,
-        password
+        password: hashedpassword
     })
 
     try {
@@ -27,8 +30,39 @@ const userSignupController = async (req, res, next) => {
     } catch (error) {
         console.log(error)
     }
-        
-    return res.status(201).json({message:user})
+
+    return res.status(201).json({ message: user })
+}
+
+
+// sign in controller
+
+const userSigninController = async (req, res, next) => {
+    const { email, password } = req.body
+
+    let existinguser
+
+    try {
+
+        existinguser = await User.findOne({ email })
+
+    } catch (error) {
+        console.log(error)
+    }
+
+    if (!existinguser) {
+        return res.status(400).json({ message: "user doesn't exist. please sign up" })
+    }
+
+    const isPasswordCorrect = bcryptjs.compareSync(password, existinguser.password)
+
+    if (!isPasswordCorrect) {
+        return res.status(400).json({ message: "invalid email or password" })
+    }
+
+    return res.status(200).json({ message: "user successfully logged in" })
+
 }
 
 exports.userSignupController = userSignupController
+exports.userSigninController = userSigninController
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express=require("express")
-const { userSignupController } = require("../controllers/userCotrollers")
+const { userSignupController, userSigninController } = require("../controllers/userCotrollers")
 
 const userRoutes=express.Router()
 
 userRoutes.post("/signup",userSignupController)
+userRoutes.post("/signin",userSigninController)
 
 module.exports=userRoutes
